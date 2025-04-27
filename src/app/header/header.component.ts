fada123f33db15c7c8f792dbd6a7c8cecaadd1ba
unknown <harshakanwar12@gmail.com>
2025-04-27 10:47:10 +0530

    basic components

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+// We are importing the Component decorator from Angular core library to create a component.
+// The Component decorator is used to define the metadata for the component, such as its selector, template, and styles.
+// The Component decorator is a function that takes an object as an argument, which contains the metadata for the component.
+// The object can contain properties such as selector, template, styles, and providers, among others.
+
+@Component(
+    {
+        selector : 'app-header',
+        standalone : true,
+        templateUrl : './header.component.html',
+        styleUrls : ['./header.component.css']
+    }
+)
+
+export class HeaderComponent {
+
+}
